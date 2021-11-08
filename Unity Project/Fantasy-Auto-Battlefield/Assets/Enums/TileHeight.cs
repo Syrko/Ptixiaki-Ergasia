fa7dc3dfbba26121f1d5a37d3f1bb499a0a13179
syrko <kostassir@yahoo.gr>
2021-11-08 17:35:07 +0200

    Imported the TileHeight and TileType enums

---
+++ Unity Project/Fantasy-Auto-Battlefield/Assets/Enums/TileHeight.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Enumeration of the different heights that the hexes of the board will have.
+/// The greatest height needs to be of value 1.
+/// </summary>
+public enum TileHeight
+{
+	High = 1,
+	Normal = 2,
+	Low = 3
+}
---
+++ Unity Project/Fantasy-Auto-Battlefield/Assets/Enums/TileType.cs
@@ -0,0 +1,12 @@
+/// <summary>
+/// Enumeration of the different terrain types available for the board's hex tiles
+/// </summary>
+public enum TileType
+{
+	Base,
+	Desert,
+	Forest,
+	Hills,
+	Lake,
+	Plains
+}
