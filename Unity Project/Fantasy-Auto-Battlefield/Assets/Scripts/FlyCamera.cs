f97597919992edc0e35f47c73e0f181b8bac2c77
syrko <kostassir@yahoo.gr>
2021-11-08 17:09:06 +0200

    Modified Camera controls to match the game's requirements (confined and only when RMB is held)

--- Unity Project/Fantasy-Auto-Battlefield/Assets/Scripts/FlyCamera.cs
+++ Unity Project/Fantasy-Auto-Battlefield/Assets/Scripts/FlyCamera.cs
@@ -5,63 +5,81 @@ public class FlyCamera : MonoBehaviour
 {
 
     /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
+    Base script Writen by Windexglow 11-13-10
     wasd : basic movement
     shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
 
+    Modified by Syrko 11-08-21
+    Made variables modifiable in the Unity Editor
+    Controls work only when the right mouse button is held
+    Confines the camera within limits
+    */
 
-    float mainSpeed = 100.0f; //regular speed
-    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    float camSens = 0.25f; //How sensitive it with mouse
+    [Header("Camera Settings")]
+    [SerializeField, Tooltip("Regular speed")]
+    float mainSpeed = 100.0f;
+    [SerializeField, Tooltip("Multiplied by how long shift is held.  Basically running")]
+    float shiftAdd = 250.0f; 
+    [SerializeField, Tooltip("Maximum speed when holdin shift")]
+    float maxShift = 1000.0f; 
+    [SerializeField, Tooltip("How sensitive it with mouse")]
+    float camSens = 0.25f;
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
+    
+    // Flag to process the first frame that the right mouse button is held down
+    private bool isMoving = false;
 
     void Update()
     {
-        lastMouse = Input.mousePosition - lastMouse;
-        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
-        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
-        lastMouse = Input.mousePosition;
-        //Mouse  camera angle done.  
-
-        //Keyboard commands
-        float f = 0.0f;
-        Vector3 p = GetBaseInput();
-        if (p.sqrMagnitude > 0)
-        { // only move while a direction key is pressed
-            if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.Mouse1))
+		{
+            // Process the Camera angle
+            if (!isMoving)
             {
-                totalRun += Time.deltaTime;
-                p = p * totalRun * shiftAdd;
-                p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-                p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-                p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+                isMoving = true;
+                lastMouse = transform.eulerAngles;
             }
             else
             {
-                totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-                p = p * mainSpeed;
+                lastMouse = Input.mousePosition - lastMouse;
+                lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+                lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
             }
+            transform.eulerAngles = lastMouse;
+            lastMouse = Input.mousePosition; 
 
-            p = p * Time.deltaTime;
-            Vector3 newPosition = transform.position;
-            if (Input.GetKey(KeyCode.Space))
-            { //If player wants to move on X and Z axis only
+            // Process the camera movent
+            Vector3 p = GetBaseInput();
+            if (p.sqrMagnitude > 0)
+            { // only move while a direction key is pressed
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    totalRun += Time.deltaTime;
+                    p *= totalRun * shiftAdd;
+                    p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+                    p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+                    p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+                }
+                else
+                {
+                    totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+                    p *= mainSpeed;
+                }
+
+                p *= Time.deltaTime;
+                Vector3 newPosition = transform.position;
                 transform.Translate(p);
-                newPosition.x = transform.position.x;
-                newPosition.z = transform.position.z;
+                newPosition.x = Mathf.Clamp(transform.position.x, -25F, 30F);
+                newPosition.y = Mathf.Clamp(transform.position.y, 2.7f, 27f);
+                newPosition.z = Mathf.Clamp(transform.position.z, -25f, 30f);
                 transform.position = newPosition;
             }
-            else
-            {
-                transform.Translate(p);
-            }
+        }
+
+		if (Input.GetKeyUp(KeyCode.Mouse1))
+		{
+            isMoving = false;
         }
     }
 
