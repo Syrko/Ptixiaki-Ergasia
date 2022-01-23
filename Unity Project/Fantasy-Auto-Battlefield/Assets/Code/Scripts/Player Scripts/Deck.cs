559e0ac862220ddbfc62f69b364d54d3e2f83334
syrko <kostassir@yahoo.gr>
2022-01-23 16:12:57 +0200

    When drawing from deck now updates UI counter

--- Unity Project/Fantasy-Auto-Battlefield/Assets/Code/Scripts/Player Scripts/Deck.cs
+++ Unity Project/Fantasy-Auto-Battlefield/Assets/Code/Scripts/Player Scripts/Deck.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 public class Deck
 {
-    private static readonly System.Random rng = new System.Random();
-
     List<string> cards;
+    BoardUI boardUI;
 
     public Deck(List<string> deck)
     {
+        boardUI = GameObject.FindObjectOfType<BoardUI>();
         cards = deck;
+        boardUI.PlayerDeckCounter.text = cards.Count.ToString();
         ShuffleDeck();
     }
 
@@ -19,6 +20,18 @@ public class Deck
     {
         string drawnCard = cards[0];
         cards.RemoveAt(0);
+
+        // Update the deck counter in the UI
+        int newDeckCount = int.Parse(boardUI.PlayerDeckCounter.text) - 1;
+        if(newDeckCount == 0)
+        {
+            // TODO implement re-shuffling
+            throw new System.NotImplementedException();
+            newDeckCount = cards.Count;
+        }
+        boardUI.PlayerDeckCounter.text = newDeckCount.ToString();
+        //---------------------------------
+
         return drawnCard;
     }
 
@@ -29,13 +42,7 @@ public class Deck
 
     void ShuffleCardsIntoDeck(List<string> newCards)
     {
-        // TODO implement
-        throw new NotImplementedException();
-    }
-
-    void GetCardsNumInDeck()
-    {
-        // TODO implement
+        // TODO implement ShuffleCardsIntoDeck of deck
         throw new NotImplementedException();
     }
 }
