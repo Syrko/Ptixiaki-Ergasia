918e3e740511cd6628c605f1aad9ecb62db5b97a
syrko <kostassir@yahoo.gr>
2022-03-10 22:49:52 +0200

    Added comments

--- Unity Project/Fantasy-Auto-Battlefield/Assets/Code/Scripts/Card Scripts/Building.cs
+++ Unity Project/Fantasy-Auto-Battlefield/Assets/Code/Scripts/Card Scripts/Building.cs
@@ -1,8 +1,18 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+/// <c>Building</c> is monobehaviour inheriting from the Spawnable class and is attached to 
+/// the gameobjects representing a Building card
+/// </summary>
 public class Building : Spawnable
 {
+    /// <summary>
+    /// Initializes a new Building with data from the appropriate Scriptable Object.
+    /// Should be called after attaching the script to a gameobject.
+    /// </summary>
+    /// <param name="buildingName">The name of the Building you are creating.</param>
+    /// <param name="forPlayer">Set as true, if the created pawn is owned by the human player.</param>
     public void InitializeBuildingPawn(string buildingName, bool forPlayer)
     {
         BuildingCardData data = BuildingCardData.GetBuildingDataFromName(buildingName);
@@ -32,6 +42,9 @@ public class Building : Spawnable
         InitializePawnUI();
     }
 
+    /// <summary>
+    /// Update the Card Info UI when clicking on the pawn.
+    /// </summary>
     private void OnMouseDown()
     {
         SubjectUI.Notify(this.gameObject, new UIEvent(EventUICodes.CARD_INFO_CHANGED));
